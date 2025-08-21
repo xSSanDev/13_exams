884fe48a93527bfe4a18ec1a9b48efe755ae2db5
anass <xssanass@gmail.com>
2025-08-21 17:33:36 -0400

    .

--- exam01/level0/count_alen/count_alen.c
+++ exam01/level0/count_alen/count_alen.c
@@ -1,14 +1,4 @@
-#include <stdio.h> // Required for printf
-#include <unistd.h>
-
-// Assuming the ft_putstr function is also available for testing
-void ft_putstr(char *str)
-{
-    while (*str)
-        write(1, str++, 1);
-}
-
-// The function to be tested
+#include <stdio.h>
 int count_alen(char *str)
 {
     int i = 0;
@@ -22,34 +12,10 @@ int count_alen(char *str)
     }
     return i;
 }
-
-int main(void)
+int main()
 {
-    char *str1 = "hello world";
-    char *str2 = "apple";
-    char *str3 = "banana";
-    char *str4 = "zebra";
-    char *str5 = "";
-    char *str6 = "Hello, I am Adam.";
-
-    printf("Testing with: \"%s\"\n", str1);
-    printf("Length up to 'a': %d\n\n", count_alen(str1)); // Expected output: 7
-
-    printf("Testing with: \"%s\"\n", str2);
-    printf("Length up to 'a': %d\n\n", count_alen(str2)); // Expected output: 0
-
-    printf("Testing with: \"%s\"\n", str3);
-    printf("Length up to 'a': %d\n\n", count_alen(str3)); // Expected output: 1
-
-    printf("Testing with: \"%s\"\n", str4);
-    printf("Length up to 'a': %d\n\n", count_alen(str4)); // Expected output: 5 (entire length)
-
-    printf("Testing with: \"%s\"\n", str5);
-    printf("Length up to 'a': %d\n\n", count_alen(str5)); // Expected output: 0
-
-    printf("Testing with: \"%s\"\n", str6);
-    printf("Length up to 'a': %d\n\n", count_alen(str6)); // Expected output: 8
-
-    return 0;
+    int result;
+    result = count_alen("Hello");
+    printf("The length is: %d\n", result);
+    return(0);
 }
-
